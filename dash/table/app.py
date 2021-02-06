84b1834c39e80b5c9e6145d6cc4377ad4781b53c
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2021-02-06 21:18:38 +0900

    update

--- dash/table/app.py
+++ dash/table/app.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
 import os
 from datetime import datetime
+from typing import cast
 
 import dash
 import dash_table
-from dash_table.Format import Format, Group, Padding
+from dash_table.Format import Format, Group, Padding, Symbol
 import dash_html_components as html
 from dash.dependencies import Input, Output
+from numpy import isin
 import pandas as pd
 import jpholiday
 
-# ----------------------------------------------------------------
+# -----------------------CONSTANTS-----------------------------------------
 
 WEEKDAYS = ["月", "火", "水", "木", "金", "土", "日"]
 HOLIDAY = "祝"
@@ -20,6 +22,7 @@ ATTENDANCE_BOOK = (
     f"{os.path.dirname(__file__)}/.data/{CURRENT_YEAR_MONTH}_attendance_book.json"
 )
 BACKUP_FILE = f"{os.path.dirname(__file__)}/.backup/{os.path.basename(ATTENDANCE_BOOK)}"
+STANDARD_WORKING_TIME = 8
 
 # ----------------------------------------------------------------
 
@@ -52,7 +55,10 @@ else:
                 曜日=HOLIDAY if is_holiday(d) else WEEKDAYS[d.weekday()],
                 開始時刻="",
                 終了時刻="",
-                TODO="",
+                休憩=1,
+                勤務時間="",
+                残業時間="",
+                進捗="",
             )
         )
     df = pd.DataFrame(data)
@@ -62,6 +68,7 @@ data = list(df.to_dict("index").values())
 app = dash.Dash(__name__)
 
 columns = []
+
 for col in df.columns:
     if col in ["日付", "曜日"]:
         columns.append(
@@ -87,7 +94,36 @@ for col in df.columns:
                 ),
             }
         )
-    else:
+    elif col in ["勤務時間"]:
+        columns.append(
+            {
+                "name": "勤務時間(＝終了-開始-休憩)",
+                "id": col,
+                "type": "numeric",
+                "editable": False,
+                "format": Format(symbol=Symbol.yes, symbol_suffix=" [h]"),
+            }
+        )
+    elif col in ["残業時間"]:
+        columns.append(
+            {
+                "name": f"残業時間(＝max(勤務時間-{STANDARD_WORKING_TIME}, 0))",
+                "id": col,
+                "type": "numeric",
+                "editable": False,
+                "format": Format(symbol=Symbol.yes, symbol_suffix=" [h]"),
+            }
+        )
+    elif col in ["休憩"]:
+        columns.append(
+            {
+                "name": col,
+                "id": col,
+                "type": "numeric",
+                "format": Format(symbol=Symbol.yes, symbol_suffix=" [h]"),
+            }
+        )
+    elif col in ["進捗"]:
         columns.append(
             {
                 "name": col,
@@ -95,18 +131,62 @@ for col in df.columns:
                 "type": "text",
             }
         )
+    else:
+        raise ValueError(f"Undefined column name {col} found")
 
 app.layout = html.Div(
     children=[
         html.H1("Attendance Book"),
         html.Button("Save", id="save-button"),
         html.Div("", id="save-status"),
+        html.H2("Summary"),
+        dash_table.DataTable(
+            id="summary-table",
+            editable=False,
+            columns=[
+                dict(
+                    name="合計勤務時間",
+                    id="合計勤務時間",
+                    type="numeric",
+                    editable=False,
+                    format=Format(symbol=Symbol.yes, symbol_suffix=" [h]"),
+                ),
+                dict(
+                    name="合計残業時間",
+                    id="合計残業時間",
+                    type="numeric",
+                    editable=False,
+                    format=Format(symbol=Symbol.yes, symbol_suffix=" [h]"),
+                ),
+            ],
+            style_data_conditional=[
+                {
+                    "if": {
+                        "column_id": ["合計残業時間"],
+                        "filter_query": "{合計残業時間} > 0",
+                    },
+                    "color": "purple",
+                },
+            ],
+            style_header=dict(
+                fontWeight="bold",
+                textAlign="center",
+                backgroundColor="rgb(230, 230, 230)",
+            ),
+            data=[dict(合計勤務時間="", 合計残業時間="")],
+        ),
+        html.H2("Table"),
         dash_table.DataTable(
             id="time-table",
             editable=True,
             columns=columns,
             data=data,
             active_cell=ACTIVE_CELL,
+            style_header=dict(
+                backgroundColor="rgb(230, 230, 230)",
+                fontWeight="bold",
+                textAlign="center",
+            ),
             style_data_conditional=[
                 {
                     "if": {
@@ -136,6 +216,7 @@ app.layout = html.Div(
                     },
                     "color": "red",
                 },
+                {"if": {"row_index": "odd"}, "backgroundColor": "rgb(248, 248, 248)"},
             ],
         ),
     ]
@@ -146,6 +227,7 @@ app.layout = html.Div(
     Output("save-status", "children"),
     Input("save-button", "n_clicks"),
     Input("time-table", "data"),
+    prevent_initial_call=True,
 )
 def saveData(n_clicks, data):
     if n_clicks:
@@ -159,20 +241,31 @@ def saveData(n_clicks, data):
 active_cell_previous = ACTIVE_CELL
 
 
+def calculate_working_hours(data):
+    for d in data:
+        try:
+            end = datetime.strptime(d["終了時刻"], "%H:%M")
+            begin = datetime.strptime(d["開始時刻"], "%H:%M")
+            break_time = d["休憩"]
+            working_hours = (end - begin).seconds / 3600
+            working_hours -= break_time
+            d["勤務時間"] = working_hours
+            d["残業時間"] = max(working_hours - STANDARD_WORKING_TIME, 0)
+        except ValueError:
+            pass
+    return data
+
+
 @app.callback(
     Output("time-table", "data"),
+    Output("summary-table", "data"),
     Input("time-table", "active_cell"),
     Input("time-table", "data_previous"),
     Input("time-table", "data"),
-    prevent_initial_call=True,
 )
 def updateData(active_cell, data_previous, data):
     global active_cell_previous
-    if active_cell_previous["column_id"] in ["Data", "曜日"]:
-        # Just upate activate_cell
-        active_cell_previous = active_cell
-        return data
-    else:
+    if active_cell_previous["column_id"] in ["開始時刻", "終了時刻"]:
         row = active_cell_previous["row"]
         column_id = active_cell_previous["column_id"]
         value = data[row][column_id]
@@ -192,7 +285,7 @@ def updateData(active_cell, data_previous, data):
             # 2345 -> 23:45
             # 9999 -> 99:99
             print("str_value", str_value)
-            try:
+            try:  # to parse time
                 str_value = ":".join([str_value[0:2], str_value[2:4]])
                 str_value = datetime.strptime(str_value, "%H:%M").strftime("%H:%M")
                 data[row][column_id] = str_value
@@ -201,8 +294,20 @@ def updateData(active_cell, data_previous, data):
                 # revert data with `previous_data`
                 data[row][column_id] = data_previous[row][column_id]
                 pass
-        active_cell_previous = active_cell
-    return data
+    data = calculate_working_hours(data)
+
+    total_overwork_working_time = total_working_time = 0
+    for d in data:
+        wt = d["勤務時間"]
+        over_wt = d["残業時間"]
+        if not isinstance(wt, str):
+            total_working_time += wt
+        if not isinstance(over_wt, str):
+            total_overwork_working_time += over_wt
+    print(total_overwork_working_time)
+    summary = [dict(合計勤務時間=total_working_time, 合計残業時間=total_overwork_working_time)]
+    active_cell_previous = active_cell
+    return data, summary
 
 
 if __name__ == "__main__":
